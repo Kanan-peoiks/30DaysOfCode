c3759e4ba3addbb047a41adc7197c04aa5f69132
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-02-11 22:51:30 +0400

    Day 19 - Queues and Stacks

---
+++ src/thirtyDaysOfCode/day19/Day19.java
@@ -0,0 +1,36 @@
+package thirtyDaysOfCode.day19;
+
+import java.util.Scanner;
+
+public class Day19 {
+    public static void main(String[] args) {
+
+        String s;
+        Scanner scan = new Scanner(System.in);
+        s = scan.nextLine();
+
+        s = s.toLowerCase();
+
+        String clean = "";
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) {
+                clean += c;
+            }
+        }
+
+        String reversed = "";
+        for (int i = clean.length() - 1; i >= 0; i--) {
+            reversed += clean.charAt(i);
+        }
+        if (s.equals(reversed)) {
+            System.out.println("The word, " + reversed + ", is a palindrome.");
+        }
+        else {
+            System.out.println("The word, " + s + ", is not a palindrome.");
+        }
+
+
+
+    }
+}
